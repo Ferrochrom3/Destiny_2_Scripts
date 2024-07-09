924614bcdd9eb85939d23f2aa93f4f1fef216dfa
Ferrochrom3 <n1476806426@gmail.com>
2024-07-08 21:39:13 -0400

    Internet Error Fix
    Added resolution_config.py
    Updated fix_internet_error() to take in a character_order string value that specifies which character to click on
    Need 1920x1080 images and config values

--- Other_Utilities/Internet_Error_Fix/internet_error_fix.py
+++ Other_Utilities/Internet_Error_Fix/internet_error_fix.py
@@ -1,14 +1,18 @@
 import time
 import pyautogui
 import keyboard
+import resolution_config
 
 """
 Additional Note
  - Currently works with The Final Shape intro screen.
- - This script chooses Hunter Human character and is using a specific emblem.
 """
 
-image_path = "Destiny_2_Scripts/Other_Utilities/Internet_Error_Fix"
+screen_width, screen_height = pyautogui.size()
+current_monitor_resolution = f"{screen_width}x{screen_height}"
+config = resolution_config.values_by_resolution[current_monitor_resolution]
+
+image_path = f"Destiny_2_Scripts/Other_Utilities/Internet_Error_Fix/Image_{current_monitor_resolution}"
 
 
 def is_internet_error():
@@ -25,11 +29,13 @@ def is_internet_error():
     return False
 
 
-def fix_internet_error():
+def fix_internet_error(character_order: str):
     """
     Exit the error code screen (which takes you to the main intro screen) and continue to character selection screen and click Hunter Human.
-    """
 
+    Args:
+        character_order (str): From top to bottom, the character to select (first, second, or third)
+    """
     print("Internet error has occured...Trying to fix it...")
     if pyautogui.locateOnScreen(f"{image_path}/Error Code Icon.png", confidence=0.8):
         keyboard.press_and_release("space")
@@ -45,12 +51,15 @@ def fix_internet_error():
             keyboard.press_and_release("space")
             break
 
-    print("Waiting to click on Hunter Human...")
+    print("Waiting to click on a character...")
     while True:
-        if pyautogui.locateOnScreen(f"{image_path}\Hunter Human.png", confidence=0.8):
-            x, y = pyautogui.locateCenterOnScreen(f"{image_path}\Hunter Human.png", confidence=0.8)
-            pyautogui.moveTo(x, y)
+        if pyautogui.locateOnScreen(f"{image_path}\Patch Notes Icon.png", confidence=0.8, region=config["patch_notes_icon_region"]):
+            time.sleep(0.3)
+            pyautogui.moveTo(config[f"{character_order}_character_coord"][0], config[f"{character_order}_character_coord"][1])
             time.sleep(0.1)
             pyautogui.leftClick()
             time.sleep(6)  # Wait 6 seconds until in game
             break
+
+
+fix_internet_error("first")
---
+++ Other_Utilities/Internet_Error_Fix/resolution_config.py
@@ -0,0 +1,15 @@
+# fmt: off
+values_by_resolution = {
+    "2560x1440": {
+        "first_character_coord": (1800, 600),
+        "second_character_coord": (1800, 750),
+        "third_character_coord": (1800, 900),
+        "patch_notes_icon_region": (2112, 115, 240, 120)
+    },
+    "1920x1080": {
+        "first_character_coord": (1300, 450),
+        "second_character_coord": (1300, 560),
+        "third_character_coord": (1300, 670),
+        "patch_notes_icon_region": (-1, -1, 240, 120)
+    },
+}
